ecb329d4eca875b5811b65ca1fbe69ba9259c3ea
lukasgjorup <SA86FJ@student.aau.dk>
2025-10-30 10:59:49 +0100

    weak commit, nothing change

--- Runneble.py
+++ Runneble.py
@@ -4,14 +4,13 @@ data.init_data(
     dataset_dir="data/",
     dataset_path="halfhourly_dataset/halfhourly_dataset/",
     file_pattern="aShortBlock_{0}.csv",
-    start_index=1,
-    end_index=2,
+    start_index=0,
+    end_index=1,
     formatted_csv="formatted_data.csv",
     weather_dataset_path="weather_hourly_darksky.csv",
     data_cols=["LCLid", "tstp", "energy(kWh/hh)"],
     data_cols_out=["LCLid", "datetime", "energy(kWh/hh)"],
     force_rebuild_data=True,
-        weather_data=["temperature"],
-    data_points = 240
+    data_points = 10000
 
 )
